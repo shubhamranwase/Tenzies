93de717c59821f976d2759eef9514ffaf26c995c
Shubham Ranwase <shubhamranwase961@gmail.com>
2026-04-04 00:32:22 +0530

    Responsiveness fix

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <>
             <nav className="flex justify-center items-center h-16 rounded-2xl m-3 shadow-[0_3px_6px_rgba(0,0,0,0.16)] bg-white">
-                <h1 className="text-3xl">Tenzies</h1>
+                <h1 className="text-4xl font-medium">Tenzies</h1>
             </nav>
         </>
     )
