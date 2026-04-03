d30584256943546f508fa0b0ee6f54e5c6ba3e9e
Shubham Ranwase <shubhamranwase961@gmail.com>
2026-04-04 00:43:12 +0530

    Responsive fix

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -47,11 +47,11 @@ export default function MainContent() {
             {gameWon && <Confetti mode="fall" particleCount={50} colors={['#ff577f', '#ff884b']} />}
             <main className="flex flex-col justify-center items-center">
                 <p className="instructions text-white font-light text-xl w-80 h-30 text-center p-3">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-                <div className="container h-100 w-120 md:mx-10 flex flex-col items-center">
+                <div className="container h-100 w-full flex flex-col items-center">
                     <div className="box p-3 bg-white rounded-2xl shadow-[0_3px_6px_rgba(0,0,0,0.16)] grid grid-cols-5 grid-rows-2 justify-items-center gap-3">
                         {diceElements}
                     </div>
-                    <button ref={buttonRef} className="w-32 h-12 font-medium bg-amber-500 hover:bg-amber-600 hover:scale-110 active:scale-95 active:bg-amber-700 transition duration-400 ease-in-out text-white text-xl py-2 px-4 rounded-xl mt-6"
+                    <button ref={buttonRef} className="w-32 h-12 font-medium bg-amber-500 hover:bg-amber-600 hover:scale-110 active:scale-95 active:bg-amber-700 transition duration-400 ease-in-out text-white text-xl py-2 px-4 rounded-xl mt-6 cursor-pointer"
                         onClick={rollDice} >{buttonText}</button>
                 </div>
             </main>
