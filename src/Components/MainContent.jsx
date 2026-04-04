e872a09ca80f1fffa6fc9a43c3cb9a02a3f7488a
Shubham Ranwase <shubhamranwase961@gmail.com>
2026-04-04 14:05:29 +0530

    Timer and Reset button added

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -16,9 +16,16 @@ export default function MainContent() {
     }
 
     const [dice, setDice] = useState(() => generateAllNewDice())
+    const [gameStarted, setGameStarted] = useState(false)
+    const [rollCount, setRollCount] = useState(0)
 
     const gameWon = dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)
-    const buttonText = gameWon ? "New Game" : "Roll Dice"
+
+    const buttonText = gameWon ? "New Game" : gameStarted ? "Roll Dice" : "Start Game"
+
+    function count() {
+        setRollCount(prevCount => prevCount + 1)
+    }
 
     const buttonRef = useRef(null)
     useEffect(() => {
@@ -27,12 +34,42 @@ export default function MainContent() {
         }
     }, [gameWon])
 
+    const [seconds, setSeconds] = useState(0)
+    const [minutes, setMinutes] = useState(0)
+
+    useEffect(() => {
+        if (gameStarted && !gameWon) {
+            const timer = setInterval(() => {
+                setSeconds(prevSeconds => {
+                    if (prevSeconds === 59) {
+                        setMinutes(prevMinutes => prevMinutes + 1)
+                        return 0
+                    }
+                    return prevSeconds + 1
+                })
+            }, 1000)
+            return () => clearInterval(timer)
+        }
+    }, [gameStarted, gameWon])
+
     function rollDice() {
+        setGameStarted(true)
+        count()
         if (!gameWon) {
             setDice((oldDice) => oldDice.map((die) => die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }))
         } else {
             setDice(() => generateAllNewDice())
+            setSeconds(0)
+            setMinutes(0)
         }
+    }
+
+    function Reset() {
+        setDice(() => generateAllNewDice())
+        setSeconds(0)
+        setMinutes(0)
+        setGameStarted(false)
+        setRollCount(0)
     }
 
     function hold(id) {
@@ -47,12 +84,18 @@ export default function MainContent() {
             {gameWon && <Confetti mode="fall" particleCount={50} colors={['#ff577f', '#ff884b']} />}
             <main className="flex flex-col justify-center items-center">
                 <p className="instructions text-white font-light text-xl w-80 h-30 text-center p-3">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-                <div className="container h-100 w-full mx-3 flex flex-col items-center">
+                <p className="timer rounded-xl bg-amber-300 w-fit h-10 px-4 text-2xl flex items-center justify-center">{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}</p>
+                <div className="container h-100 w-full mx-3 mt-6 flex flex-col items-center">
                     <div className="box p-3 bg-white rounded-2xl shadow-[0_3px_6px_rgba(0,0,0,0.16)] grid grid-cols-5 grid-rows-2 justify-items-center gap-3">
                         {diceElements}
                     </div>
-                    <button ref={buttonRef} className="w-fit h-12 font-medium bg-amber-500 hover:bg-amber-600 hover:scale-110 active:scale-95 active:bg-amber-700 transition duration-400 ease-in-out text-white text-xl py-2 px-4 rounded-xl mt-6 cursor-pointer"
-                        onClick={rollDice} >{buttonText}</button>
+                    <div className="buttons flex items-center justify-center gap-3">
+                        <p className="counter w-fit h-12 font-medium bg-amber-500 transition duration-400 ease-in-out text-white text-2xl py-2 px-4 rounded-xl mt-6 cursor-pointer">{rollCount}</p>
+                        <button ref={buttonRef} className="w-fit h-12 font-medium bg-amber-500 hover:bg-[rgb(255,140,0)] hover:scale-110 active:scale-95 active:bg-amber-600 transition duration-400 ease-in-out text-white text-2xl py-2 px-4 rounded-xl mt-6 cursor-pointer"
+                            onClick={rollDice} >{buttonText}</button>
+                        <button className="w-fit h-12 font-medium bg-[rgb(255,68,68)] hover:bg-red-500 hover:scale-110 active:scale-95 active:bg-red-700 transition duration-400 ease-in-out text-white text-2xl py-2 px-4 rounded-xl mt-6 cursor-pointer"
+                            onClick={Reset} >↺</button>
+                    </div>
                 </div>
             </main>
         </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./main.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
